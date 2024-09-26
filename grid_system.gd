06d8390c9a5fa7feb5fb50b26930f47e4b4748d1
Aidas Litvinas <aidlit@ktu.lt>
2024-09-26 05:10:53 +0300

    added astar pathfinding

--- grid_system.gd
+++ grid_system.gd
@@ -1,34 +1,72 @@
 extends Node2D
+class_name grid_navigation_system
+
+### astar needs to store cells as integer ids
+### meaning we have to extract the location from id when needed and vice versa
+### 
+### id to location: Vector2i(astargrid.get_point_position(id))
+### location to id: cells.get(location)
+### 
+### note 1, astargrid.get_point_position(id) returns a Vector2, not a Vector2i
+### therefore we convert it on the spot.
+### 
+### note 2, cells[location] can also work, but it's a getter and setter in one,
+### making it possible to modify what should be an immutable value
+
+var _visited_cells : Array[Vector2i]
+var _queue_cells : Array[Vector2i]
 
 @onready var base_layer: TileMapLayer = $"base-layer"
 var astargrid = AStar2D.new()
-var visited_cells : Array[Vector2i]
-var cell_queue : Array[Vector2i]
+var cells : Dictionary
 
 func _ready() -> void:
-	setup_astar()
-
-func _process(delta: float) -> void:
-	pass
-
-# only BFS done, just need to take the results and create the graph for astar
-func setup_astar():
-	var start_cell = Vector2i(-1, -1)
+	cells.clear()
+	_setup_astar()
 	
-	visited_cells.append(start_cell)
-	BFS(start_cell)
-	print(visited_cells)
-	
-	for node : Vector2i in visited_cells:
-		base_layer.set_cell(node, 1, Vector2i(2, 1))
+	print(cells)
 
-func BFS(current_cell : Vector2i):
+func _setup_astar():
+	var start_cell = Vector2i(0, 0)
+	
+	_create_cell(0, start_cell)
+	
+	_BFS(start_cell)
+
+func _BFS(current_cell : Vector2i):
 	for neighbor : Vector2i in base_layer.get_surrounding_cells(current_cell):
 		if base_layer.get_cell_tile_data(neighbor):
-			if base_layer.get_cell_tile_data(neighbor).get_custom_data("walkable"):
-				if !visited_cells.has(neighbor):
-					visited_cells.append(neighbor)
-					cell_queue.append(neighbor)
+			if navigation_check(neighbor):
+				if !_visited_cells.has(neighbor):
+					_queue_cells.append(neighbor)
+					_create_cell(_visited_cells.size(), neighbor)
+				
+				astargrid.connect_points(cells[neighbor], cells[current_cell])
 	
-	if cell_queue:
-		BFS(cell_queue.pop_front())
+	if _queue_cells:
+		_BFS(_queue_cells.pop_front())
+
+func _create_cell(id : int, location : Vector2i):
+	_visited_cells.append(location)
+	astargrid.add_point(id, location)
+	cells[location] = id
+
+# can this cell be traveled to
+func navigation_check(target_cell):
+	var walkable : bool = false
+	walkable = base_layer.get_cell_tile_data(target_cell).get_custom_data("walkable")
+	return walkable
+
+func get_navigation_path(from_cell : Vector2i, target_cell : Vector2i) -> Array[Vector2i]:
+	# return empty path is the destination is invalid
+	if not cells.has(target_cell):
+		#print("invalid target")
+		return []
+	
+	var path = astargrid.get_id_path(cells[from_cell], cells[target_cell])
+	var index_path : Array[Vector2i] = []
+	
+	for step in path:
+		index_path.append(Vector2i(astargrid.get_point_position(step)))
+	
+	return index_path
--- unit_test.gd
+++ unit_test.gd
@@ -1,37 +1,39 @@
 extends CharacterBody2D
 
 @export var tilemap_layer: TileMapLayer
+@export var grid_system: grid_navigation_system
+
+var current_cell : Vector2i
+
+func _ready() -> void:
+	goto_cell(Vector2i(0, 0))
 
 # self documenting code amirite
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.pressed:
-			var target_pos = get_pressed_tile_pos(event.position)
+			# local_to_map translates our mouse click position into a tilemap position
+			var target_cell : Vector2i = tilemap_layer.local_to_map(event.position)
 			
-			# tween is animation stuffs, temporary code here
-			var tween = get_tree().create_tween()
-			tween.tween_property(self, "global_position", target_pos, 0.5)
+			var path = grid_system.get_navigation_path(current_cell, target_cell)
+			
+			if not path.is_empty():
+				travel_path(path)
 
-# ignore this, it's for physics stuff
-func _physics_process(delta: float) -> void:
-	pass
+func travel_path(path):
+	for next_step in path:
+		# await needs to happen inside this loop
+		# if it's in a seperate function, the looped functions will be executed in parallel, which is not what we want
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "global_position", tilemap_layer.map_to_local(next_step), 0.1)
+		await tween.finished
+		
+		current_cell = next_step
 
-func get_pressed_tile_pos(press_location: Vector2) -> Vector2:
-	# gets tile position of within the tilemap (works somewhat like an index)
-	# tilemap position =/= global position
-	var tile_pos = tilemap_layer.local_to_map(press_location)
+# travels to a cell, for singular use only
+func goto_cell(target : Vector2i):
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "global_position", tilemap_layer.map_to_local(target), 0.2)
+	await tween.finished
 	
-	# if tile exists on clicked location, we go there.
-	# otherwise we stay in the current location
-	var tile = tilemap_layer.get_cell_tile_data(tile_pos)
-	if tile:
-		# if tile is walkable, we go there.
-		# otherwise we stay in the current location
-		if tile.get_custom_data("walkable"):
-			return tilemap_layer.map_to_local(tile_pos)
-		else:
-			#TODO bad practice or something
-			return self.global_position
-	else:
-		#TODO bad practice or something
-		return self.global_position
+	current_cell = target
